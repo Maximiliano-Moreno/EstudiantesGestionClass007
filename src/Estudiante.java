35e4ec8d94e7e29845f89ac22adbea41e4a90462
Maximiliano-Moreno <maximilianomoreno0291@gmail.com>
2025-10-03 16:48:01 -0600

    Estudiante.java

---
+++ src/Estudiante.java
@@ -0,0 +1,22 @@
+public class Estudiante {
+    private String nombre;
+    private int edad;
+    private double calificacion;
+
+    public Estudiante(String nombre, int edad, double calificacion) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.calificacion = calificacion;
+    }
+
+    public void mostrarInformacion() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad);
+        System.out.println("Calificación: " + calificacion);
+    }
+
+    public void setCalificacion(double calificacion) {
+        this.calificacion = calificacion;
+    }
+}
+
